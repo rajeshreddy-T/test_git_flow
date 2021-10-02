b5f2f4bb93bbe65147952a8ef36ac4d0143bf252
Rajesh Reddy Thiyyagura <raj30498@gmail.com>
2021-10-02 10:52:56 +0530

    initial commit with starter code

---
+++ src/main/java/com/rajeshreddy/restapi/RestapiApplication.java
@@ -0,0 +1,15 @@
+package com.rajeshreddy.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("hi I am rajesh reddy");
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+}
